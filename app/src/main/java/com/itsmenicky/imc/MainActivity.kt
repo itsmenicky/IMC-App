136717f843b1446e5620436df33ec071c12af29c
slannec <nick104@outlook.com>
2024-11-10 05:07:15 -0300

    calculando IMC

--- app/src/main/java/com/itsmenicky/imc/MainActivity.kt
+++ app/src/main/java/com/itsmenicky/imc/MainActivity.kt
@@ -12,5 +12,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val btn_calc = binding.btnCalcular
+        val resultado = binding.textViewResultado
+
+        btn_calc.setOnClickListener{
+            val peso = binding.editTxtPeso.text.toString()
+            val altura = binding.editTxtAltura.text.toString()
+
+            if(peso.isEmpty()){
+                resultado.setText("Informe seu peso!")
+            }else if(altura.isEmpty()){
+                resultado.setText("Informe sua altura!")
+            }else{
+                calculaIMC(peso, altura)
+            }
+        }
+    }
+    private fun calculaIMC(peso:String, altura:String){
+        val peso = Integer.parseInt(peso)
+        val altura = Integer.parseInt(altura)
+        val resultado = binding.textViewResultado
+        val imc = peso / (altura * altura)
+
+        val mensagem = when{
+            imc <= 18.5 -> "Peso Baixo"
+            imc <= 24.9 -> "Peso Normal"
+            imc <= 29.9 -> "Sobrepeso"
+            imc <= 34.9 -> "Obesidade (Grau I)"
+            imc <= 39.9 -> "Obesidade (Grau II)"
+            else -> "Obesidade Mórbida"
+        }
+
+        imc.toString()
+        resultado.setText("$imc \n $mensagem")
     }
 }
